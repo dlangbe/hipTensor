e29ff59d88839e8a58aa4b6cb4a5328b4c5ed3f3
dlangbe <David.Langbehn@amd.com>
2025-05-17 01:35:41 +0000

    merge changes

--- library/include/hiptensor/hiptensor_types.hpp
+++ library/include/hiptensor/hiptensor_types.hpp
@@ -106,7 +106,6 @@ typedef enum
     HIPTENSOR_STATUS_INSUFFICIENT_DRIVER = 20,
     //! Indicates an error related to file I/O.
     HIPTENSOR_STATUS_IO_ERROR = 21,
-
 } hiptensorStatus_t;
 
 //! @brief hipTensor compute type enumeration
